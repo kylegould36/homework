a2e3778a9d7565f60f0f35c80f1f37273bb00985
Kyle <gouldielox33@gmail.com>
2023-07-27 13:29:03 -0400

    Where is waldo answered

---
+++ answers.js
@@ -0,0 +1,75 @@
+//Easy Going
+for(let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+//Get Even
+for(let i = 0; i <= 200; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+//Fizz Buzz
+for(let i = 1; i <= 100; i++) {
+    if(i % 3 == 0) {
+        console.log("Fizz");
+    }
+    if(i % 5 == 0) {
+        console.log("Buzz");
+    }
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+}
+
+//Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = plantee[2]+1;
+console.log(plantee[2]);
+wolfy[3] = "Gotham City";
+console.log(wolfy[3]);
+dart.push("Hawkins");
+console.log(dart);
+wolfy[0] = "Gameboy";
+console.log(wolfy);
+
+//Yell at the Ninja Turtles
+const turtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+for(let ninjaTurtles of turtles) {
+    console.log(ninjaTurtles.toUpperCase());
+}
+
+//favMovies
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+console.log(favMovies.indexOf("Titanic"));
+favMovies.sort();
+favMovies.pop();
+favMovies.push("Guardians of the Galaxy");
+favMovies.reverse();
+favMovies.shift();
+favMovies.unshift("Good Will Hunting");
+console.log(favMovies.indexOf("Django Unchained"));
+favMovies.splice(15, 1, "Avatar");
+//Cut favMovies in half
+const halfMovies = favMovies.length / 2;
+//Set halfMovies side to someMovies
+const someMovies = favMovies.slice(0, halfMovies);
+console.log(someMovies);
+console.log(favMovies.indexOf("Fast and Furious"));
+
+//Where is Waldo
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+whereIsWaldo.splice(1, 1);
+whereIsWaldo[1][2] = "No One";
+console.log(whereIsWaldo[2][1][1]);
+
+
+
