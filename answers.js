efe29aea327857359a3b092d71f55df60639ddc8
Kyle <gouldielox33@gmail.com>
2023-07-27 14:00:51 -0400

    Find the median answered

--- answers.js
+++ answers.js
@@ -86,5 +86,3 @@ for(let i = 0; i <= 20; i++) {
 const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
 nums.sort();
 console.log(nums[Math.floor(nums.length / 2)]);
-
-
